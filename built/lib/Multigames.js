ca799a1681f0357b91fd905ce218ff743fd87c49
leonardoserra <serra.leonardo92@gmail.com>
2026-09-17 21:47:33 +0200

    removed the built dir from the push

--- built/lib/Multigames.js
+++
@@ -1,9 +0,0 @@
-import Engine from "./core/Engine.js";
-import Rectangle, { RectangleState } from "./Rectangle.js";
-class Multigames {
-    static init() {
-        const engine = new Engine();
-        engine.entities.push(new Rectangle(new RectangleState(100, 100, 300, 200, 0.01)));
-    }
-}
-Multigames.init();
